36aae6de6d8e996f57bd0ab1c1f3172dbebc3d87
shyam_kumar_sohane@dell.com <shyam_kumar_sohane@dell.com>
2023-02-11 12:13:30 -0600

    added utctime package.
    publishing to github

---
+++ src/Serilog.Enrichers.UtcTime/Enrichers/UtcTimeEnricher.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+
+namespace Serilog.Enrichers.UtcTime.Enrichers
+{
+    /// <summary>
+    /// Enriches log events with an UtcTime property for the current request.
+    /// </summary>
+    class UtcTimeEnricher
+        : ILogEventEnricher
+    {
+        private const string UtcTimePropertyName = "UtcTimeStamp";
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public UtcTimeEnricher() : this(new HttpContextAccessor())
+        {
+        }
+
+        public UtcTimeEnricher(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+
+        /// <summary>
+        /// Enrich the log event.
+        /// </summary>
+        /// <param name="logEvent">The log event to enrich.</param>
+        /// <param name="propertyFactory">Factory for creating new properties to add to the event.</param>
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var property = propertyFactory.CreateProperty(UtcTimePropertyName, DateTime.UtcNow.ToString("O") ?? "-");
+            logEvent.AddOrUpdateProperty(property);
+        }
+    }
+}
---
+++ src/Serilog.Enrichers.UtcTime/UtcTimeLoggerConfigurationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Enrichers.UtcTime.Enrichers;
+
+namespace Serilog
+{
+    /// <summary>
+    /// Extends <see cref="LoggerConfiguration"/> to add enrichers for the current UtcTime/>.
+    /// capabilities.
+    /// </summary>
+    public static class UtcTimeLoggerConfigurationExtensions
+    {
+        /// <summary>
+        /// Enrich log events with a UtcTime property containing the current UtcTime/>.
+        /// </summary>
+        /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration WithUtcTime(
+           this LoggerEnrichmentConfiguration enrichmentConfiguration)
+        {
+            if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
+            return enrichmentConfiguration.With<UtcTimeEnricher>();
+        }
+    }
+}
